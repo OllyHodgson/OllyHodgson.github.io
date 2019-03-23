7518fc16c1c4b7544e68b0702c10143a8c0c9855
Olly Hodgson <olly@thinkdrastic.net>
2019-03-23 16:07:31 -0700

    Added a projects page and cleaned up a bit.

--- media/script/library.js
+++ media/script/library.js
@@ -1,108 +1,104 @@
 /*global $*/
 /*global jQuery*/
 
-/*
-
-Is the element offscreen?
-Shamelessly copied from https://stackoverflow.com/a/8897628
-
-*/
-jQuery.expr.pseudos.offscreen = function(element) {
-  var rect = element.getBoundingClientRect();
+// Custom selector to find if something is in the viewport
+// Adapted from https://stackoverflow.com/a/8897628/13019
+jQuery.expr.pseudos.offscreen = function(el) {
+  var rect = el.getBoundingClientRect();
   return (
     rect.x + rect.width < 0 ||
     rect.y + rect.height < 0 ||
-    rect.x > window.innerWidth ||
-    rect.y > window.innerHeight
+    (rect.x > window.innerWidth || rect.y > window.innerHeight)
   );
 };
 
 /*
-
-jQ plugin to transition elements when scrolled into view
-
-*/
-(function($, window) {
+ * ollyTransition plugin
+ * Olly Hodgson, still writing jQuery plugins in November 2018
+ *
+ * Usage:
+ *
+ * Add class="olly-transition olly-transition-hidden" to the elements you want to fade in
+ * Add class="olly-transition-group" to their parent element
+ *
+ * Optionally, add data-timeout="1000" data-timeout-increment="500" (you can change the numbers)
+ * to the olly-transition-group element to provide a custom delay to the transitions
+ *
+ * You define the actual transitions in your CSS, e.g.
+ *
+ * .example-element.olly-transition {
+ *     opacity: 1;
+ *     transition: all 0.5s ease-out;
+ * }
+ * .example-element.olly-transition-hidden {
+ *     opacity: 0;
+ * }
+ *
+ */
+(function($) {
   "use strict";
 
-  // Create the defaults once
-  var pluginName = "ohTransitionGroup",
-    defaults = {
-      timeout: 750,
-      timeoutIncrement: 250,
-      itemSelector: ".oh-transition",
-      itemHiddenSelector: ".oh-transition-hidden"
-    };
-
-  // The actual plugin constructor
-  function Plugin(element, options) {
-    this.element = element;
-
-    // jQuery has an extend method that merges the
-    // contents of two or more objects, storing the
-    // result in the first object. The first object
-    // is generally empty because we don't want to alter
-    // the default options for future instances of the plugin
-    this.options = $.extend({}, defaults, options);
-
-    this._defaults = defaults;
-    this._name = pluginName;
-
-    this.init();
-  }
-
-  Plugin.prototype = {
-    init: function() {
-      // Place initialization logic here
-      // You already have access to the DOM element and
-      // the options via the instance, e.g. this.element
-      // and this.options
-      // you can add more functions like the one below and
-      // call them like so: this.yourOtherFunction(this.element, this.options).
-      var that = this;
-      $(this.element).each(function(i, el) {
-        var opts = that.options,
-          $el = $(el),
-          timeout = $el.data("timeout"),
-          n = $el.data("timeoutIncrement");
-        if (timeout !== 0 && timeout !== "") {
-          opts.timeout = timeout;
-        }
-        if (n !== 0 && n !== "") {
-          opts.timeoutIncrement = n;
-        }
-        var $relevantElements = $el.find(opts.itemSelector).filter(opts.itemHiddenSelector);
-
-        that.checkIfOnScreen(opts, $relevantElements);
-        $(window).scroll(function() {
-          that.checkIfOnScreen(opts, $relevantElements);
-        });
+  // Set up the accordion navigation
+  $.fn.ollyTransitionItems = function(options) {
+    // This is the easiest way to have default options.
+    var settings = $.extend({}, $.fn.ollyTransitionItems.defaults, options);
+    var $groups = $(this);
+    $groups.each(function() {
+      var $group = $(this);
+      var customTimeout = $group.data("timeout");
+      var customTimeoutIncrement = $group.data("timeoutIncrement");
+      if (customTimeout !== undefined && customTimeout !== "") {
+        settings.timeout = customTimeout;
+      }
+      if (
+        customTimeoutIncrement !== undefined &&
+        customTimeoutIncrement !== ""
+      ) {
+        settings.timeoutIncrement = customTimeoutIncrement;
+      }
+      var items = $group
+        .find(settings.itemSelector)
+        .filter(settings.itemHiddenSelector);
+      // Do the transition immediately for things which are on screen
+      doTransition(settings, items);
+      // Do the transition on scroll for things which are not on screen
+      $(window).scroll(function() {
+        doTransition(settings, items);
       });
-    },
-
-    checkIfOnScreen: function(opts, el) {
-      for (var n = opts.timeout, i = 0; i < el.length; i++) {
-        $(el[i]).is(":offscreen") ||
-          ((n += opts.timeoutIncrement),
-          setTimeout(this.r, n, el[i], opts.itemHiddenSelector));
-      }
-    },
-
-    r: function(t, e) {
-      "." === e.charAt(0) && (e = e.substr(1)), $(t).removeClass(e);
-    }
-  };
-
-  // A really lightweight plugin wrapper around the constructor,
-  // preventing against multiple instantiations
-  $.fn[pluginName] = function(options) {
-    return this.each(function() {
-      if (!$.data(this, "plugin_" + pluginName)) {
-        $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-      }
     });
   };
-})(jQuery, window);
+
+  // These are the defaults.
+  $.fn.ollyTransitionItems.defaults = {
+    timeout: 750,
+    timeoutIncrement: 250,
+    itemSelector: ".olly-transition",
+    itemHiddenSelector: ".olly-transition-hidden"
+  };
+
+  // This removes the settings.itemHiddenSelector class from the elements in items after settings.timeout is reached
+  // If there is more than one element, it'll loop through them, starting the next one after settings.timeout + settings.timeoutIncrement is reached
+  function doTransition(settings, items) {
+    var timeout = settings.timeout;
+    for (var i = 0; i < items.length; i++) {
+      // :offscreen is a custom selector, see custom.js
+      if (!$(items[i]).is(":offscreen")) {
+        timeout = timeout + settings.timeoutIncrement;
+        //console.info(i, items[i], timeout);
+        setTimeout(showItem, timeout, items[i], settings.itemHiddenSelector);
+      }
+    }
+  }
+
+  // Remove the className, which (via the magic of CSS transition) shows the item(s)
+  function showItem(el, selector) {
+    //console.info(el, selector);
+    if (selector.charAt(0) === ".") {
+      selector = selector.substr(1);
+    }
+    $(el).removeClass(selector);
+  }
+})(jQuery);
 
 /* ********************
 
@@ -111,5 +107,5 @@ The onload function.
 ******************** */
 
 $(document).ready(function() {
-  $(".oh-transition-group").ohTransitionGroup();
+  $(".olly-transition-group").ollyTransitionItems();
 });
